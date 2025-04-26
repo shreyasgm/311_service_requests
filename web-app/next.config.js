c3638499b241fd63bfd99c936a202ac66c634854
Robert Christie <rgcgeog@gmail.com>
2025-04-26 12:01:28 -0400

    Update Next.js configuration and package versions

---
+++ web-app/next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'standalone',
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    unoptimized: true
+  }
+};
+
+module.exports = nextConfig; 
